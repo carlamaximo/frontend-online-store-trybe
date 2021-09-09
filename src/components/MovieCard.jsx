ee81dca232f33a3b6f6d8f996e334c82a4721761
Carla <carladmaximo@gmail.com>
2021-09-09 20:01:23 -0300

    Requisitos 11, 12, 13 e 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Rating from './Rating';
+
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <section>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-      </section>);
+        <Rating rating={ rating } />
+      </section>
+    );
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,6 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    // console.log(movies);
 
     return (
       <div>
